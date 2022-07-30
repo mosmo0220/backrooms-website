0e917c9f18a38a19d6eeb82aa73c43ede71f42b4
kubek420 <kaudekbog@gmail.com>
2022-07-30 21:57:17 +0200

    Added session

--- Data/dbconn.cs
+++ Data/dbconn.cs
@@ -6,13 +6,16 @@ namespace Backrooms.Data {
     public class RoomsData {
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-        public ObjectId id { get; set; }
+        public ObjectId id { get; set; } 
         public string name { get; set; }
         public string owner { get; set; }
     }
-    public class SchemaData {
+    public class Session {
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public ObjectId id { get; set; }
+        public string token { get; set; }
+        public UserData user { get; set; }
+        public List<RoomsData> rooms { get; set; }
     }
 }
--- Data/dbconnService.cs
+++ Data/dbconnService.cs
@@ -7,13 +7,27 @@ namespace Backrooms.Data {
     public class dbconnService {
         public async Task<List<RoomsData>> GetRoomsData(string id) {
             var client = new MongoClient(Environment.GetEnvironmentVariable("MONGODB_URI"));
-            /var client = new MongoClient("mongodb+srv://mosmo0220:rf6w29Drdbhie1pl@cluster0.0aenioa.mongodb.net/?retryWrites=true&w=majority");
             var database = client.GetDatabase("DiscordBackrooms");
-            Console.WriteLine(database.ListCollectionsAsync().Result.ToList().ToJson());
             var collection = database.GetCollection<RoomsData>("rooms");
             var filter = Builders<RoomsData>.Filter.Eq("owner", id);
             var result = await collection.Find(filter).ToListAsync();
-            Console.WriteLine(result.ToJson());
+            return result;
+        }
+        public async Task SaveSession(Session session) {
+            var client = new MongoClient(Environment.GetEnvironmentVariable("MONGODB_URI"));
+            var database = client.GetDatabase("DiscordBackrooms");
+            var collectionExists = database.ListCollectionNames().ToList().Contains("sessions");
+            if(!collectionExists) {
+                database.CreateCollection("sessions");
+            }
+            database.GetCollection<Session>("sessions").InsertOne(session);
+        }
+        public async Task<Session> GetSession(string token) {
+            var client = new MongoClient(Environment.GetEnvironmentVariable("MONGODB_URI"));
+            var database = client.GetDatabase("DiscordBackrooms");
+            var collection = database.GetCollection<Session>("sessions");
+            var filter = Builders<Session>.Filter.Eq("token", token);
+            var result = await collection.Find(filter).FirstOrDefaultAsync();
             return result;
         }
     }
