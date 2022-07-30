99921eb6d8a077f2bdce15e86472f4c1ccb0adfe
kubek420 <kaudekbog@gmail.com>
2022-07-30 22:14:06 +0200

    Added configure subpage

--- Data/dbconn.cs
+++ Data/dbconn.cs
@@ -16,6 +16,5 @@ namespace Backrooms.Data {
         public ObjectId id { get; set; }
         public string token { get; set; }
         public UserData user { get; set; }
-        public List<RoomsData> rooms { get; set; }
     }
 }
